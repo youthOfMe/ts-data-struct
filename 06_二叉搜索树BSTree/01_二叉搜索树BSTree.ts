644f448bf32a6525e9833316a4568288d6c67b12
youthOfMe <2040690180@qq.com>
2024-07-14 22:46:49 +0800

    feat: 封装实现二叉搜索树的增加和查找节点

--- 06_二叉搜索树BSTree/01_二叉搜索树BSTree.ts
+++ 06_二叉搜索树BSTree/01_二叉搜索树BSTree.ts
@@ -4,6 +4,18 @@ import Node from "../types/INode"
 class TreeNode<T> extends Node<T> {
   left: TreeNode<T> | null = null
   right: TreeNode<T> | null = null
+
+  parent: TreeNode<T> | null = null
+
+  // 判断当前节点是父节点的左子节点
+  get isLeft(): boolean {
+    return !!(this.parent && this.parent.left === this)
+  }
+
+  // 判断当前节点是父节点的右子节点
+  get isRight(): boolean {
+    return !!(this.parent && this.parent.right === this)
+  }
 }
 
 class BSTree<T> {
@@ -46,6 +58,35 @@ class BSTree<T> {
       this.insertNode(this.root, newNode)
     }
   }
+
+  /** 根据值进行搜索节点 */
+  private searchNode(value: T): TreeNode<T> | null {
+    let current = this.root
+    let parent: TreeNode<T> | null = null
+
+    while (current) {
+      // 1. 如果找到current直接返回即可
+      if (current.value === value) return current
+
+      // 继续向下走
+      parent = current
+      if (current.value < value) {
+        current = current.right
+      } else {
+        current = current.left
+      }
+
+      // 如果current有值, 那么current保存自己的父节点
+      if (current) current.parent = parent
+    }
+
+    return null
+  }
+
+  /** 搜索特定的值: 20 => boolean */
+  search(value: T): boolean {
+    return !!this.searchNode(value)
+  }
 }
 
 const bst = new BSTree<number>()
@@ -53,7 +94,16 @@ const bst = new BSTree<number>()
 bst.insert(20)
 bst.insert(30)
 bst.insert(18)
+bst.insert(21)
+bst.insert(32)
+bst.insert(12)
+bst.insert(15)
+bst.insert(23)
+bst.insert(19)
 
 bst.print()
+
+console.log(bst.search(19))
+console.log(bst.search(88))
 
 export { }
